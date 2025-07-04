c7a80cd7c34fe2aa89121c68d75ac5f6471cd8dd
Yiftah Waisman <yiftahw1@gmail.com>
2025-07-04 10:17:30 +0300

    litgen example has multiple inputs combined to a single output

---
+++ litgen/line.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <cmath>
+#include "pose.hpp"
+class Line {
+public:
+    Line(Pose p1, Pose p2) : p1(p1), p2(p2) {}
+
+    /// Returns the length of the line segment.
+    float length() const {
+        auto [x1, y1] = p1.get_values();
+        auto [x2, y2] = p2.get_values();
+        return std::sqrt(std::pow(x2 - x1, 2) + std::pow(y2 - y1, 2));
+    }
+
+    // @returns slope of the line segment
+    // @note might return std::numeric_limits<float>::infinity() if the line is vertical
+    float m() const {
+        auto [x1, y1] = p1.get_values();
+        auto [x2, y2] = p2.get_values();
+        if (x2 - x1 == 0) {
+            return std::numeric_limits<float>::infinity(); // vertical line
+        }
+        return (y2 - y1) / (x2 - x1);
+    }
+
+    // @returns y-intercept of the line segment
+    // @note might return std::numeric_limits<float>::infinity() if the line is vertical
+    float n() const {
+        auto [x1, y1] = p1.get_values();
+        auto [x2, _] = p2.get_values();
+        if (x2 - x1 == 0) {
+            return std::numeric_limits<float>::infinity(); // vertical line
+        }
+        return y1 - m() * x1;
+    }
+
+    std::string to_string() const {
+        return "y = " + std::to_string(m()) + " * x + " + std::to_string(n());
+    }
+private:
+    Pose p1, p2;
+};
--- litgen/pose.hpp
+++ litgen/pose.hpp
@@ -4,25 +4,27 @@
 
 class Pose {
 public:
-    Pose() : x(0), y(0), z(0) {}
+    Pose() : x(0), y(0) {}
 
     /// this is a function description  
     // returns void  
     // param x some x value  
     // param y some y value  
-    // param z some z value  
-    void set_values(float x, float y, float z) {
+    void set_values(float x, float y) {
         this->x = x; 
         this->y = y; 
-        this->z = z;
+    }
+
+    // returns a tuple of the values x,y
+    std::tuple<float, float> get_values() const {
+        return std::make_tuple(x, y);
     }
 
     std::string to_string() const {
         return "Pose(" + std::to_string(x) + ", " + 
-                         std::to_string(y) + ", " +
-                         std::to_string(z) + ")";
+                         std::to_string(y) + ")";
     }
 
 private:
-    float x, y, z;
+    float x, y;
 };
